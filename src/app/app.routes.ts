f6a99590c118126063884fa9d67d42deb836733f
Dy-Rongrath <dyrongrath@gmail.com>
2024-12-30 17:05:00 +0700

    design template create account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingComponent } from './resources/landing/component';
 import { LoginPageComponent } from './resources/login/component';
 import { DashboardPageComponent } from './resources/dashboard/component';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { SignUpPageComponent } from './resources/signup/component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
   { path: 'landing-page', component: LandingComponent},
   { path: 'login-page', component: LoginPageComponent},
+  { path: 'signup-page', component: SignUpPageComponent },
   { path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard] },
 ];
