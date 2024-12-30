a1691d281f73a5a093e5070026e02be0cd507637
Dy-Rongrath <dyrongrath@gmail.com>
2024-12-30 16:29:01 +0700

    fix

---
+++ src/app/resources/sidebar/component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'sidebar-component',
+  templateUrl: 'template.html',
+  styleUrls: ['style.css']
+})
+
+export class SidebarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
