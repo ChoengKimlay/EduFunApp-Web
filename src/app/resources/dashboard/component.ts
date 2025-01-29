df1daabb7c58ff3a75a4c49d9c8f0a681f595384
Dy-Rongrath <dyrongrath@gmail.com>
2025-01-29 15:05:39 +0700

    dashboard

--- src/app/resources/dashboard/component.ts
+++ src/app/resources/dashboard/component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../layout/sidebar/component';
 import { HeaderComponent } from '../layout/header/component';
 import { DashboardService } from './service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'dashboard-page',
@@ -9,11 +10,14 @@ import { DashboardService } from './service';
     templateUrl: 'template.html',
     imports: [
         SidebarComponent,
-        HeaderComponent
+        HeaderComponent,
+        CommonModule,
     ],
 })
 
 export class DashboardPageComponent implements OnInit {
+
+    isSidebarOpen = false;
 
     constructor(private service: DashboardService) { }
 
@@ -21,4 +25,7 @@ export class DashboardPageComponent implements OnInit {
 
     }
 
+    toggleSidebar() {
+        this.isSidebarOpen = !this.isSidebarOpen;
+    }
 }
