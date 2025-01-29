c0b7f4709db9c5122605bc6cf2b6f58314a286ff
Dy-Rongrath <dyrongrath@gmail.com>
2025-01-29 16:16:19 +0700

    design sign up

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,6 @@
 export const env = {
-    api: 'http://localhost:5000/api',
-    ws: 'ws://localhost:5000'
+    api: 'http://localhost:3000/api',
+    ws: 'ws://localhost:3000'
 }
 
 // should really get those url from env file instead but i am lazy. u shouldn't though as it is best practice
