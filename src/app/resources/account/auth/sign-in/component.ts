aa27d839763271f034255b60511c3bd9fc8e2308
Kang Eangchheang <eangchheangkang@gmail.com>
2025-02-07 03:51:52 +0700

    cofltic mege

--- src/app/resources/account/auth/sign-in/component.ts
+++ src/app/resources/account/auth/sign-in/component.ts
@@ -1,7 +1,5 @@
 // Core Angular
 import { Component, OnDestroy, OnInit }                              from '@angular/core';
-import { CommonModule }                                              from '@angular/common';
-import { HttpClient }                                                from '@angular/common/http';
 import { Router, RouterModule }                                      from '@angular/router';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -18,6 +16,10 @@ import { ErrorHandleService } from 'app/helper/error-handle.service';
 import { SnackbarService } from 'app/helper/snack-bar.service';
 import GlobalConstants from 'app/helper/constants';
 import { UserService } from 'app/core/user/user.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector    : 'auth-sign-in',
